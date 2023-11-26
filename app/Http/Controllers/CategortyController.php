01c1625083569cebe45e997bb4bb3082e0e6cd6a
Rothy99 <rothyyorn9@gmail.com>
2023-11-26 09:17:53 +0700

    first commit

---
+++ app/Http/Controllers/CategortyController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategortyController extends Controller
+{
+    public function List_Category(){
+
+        $category=[
+            
+                (object) [
+                    'id' => 1,
+                    'cat_id' => 'S001',
+                    'cat_name'=>'Soft drink',
+                    'desc'=>'testing'
+                ],
+                (object) [
+                    'id' => 2,
+                    'cat_id' => 'S002',
+                    'cat_name'=>'Coffee',
+                    'desc'=>'testing'
+                ],
+                (object) [
+                    'id' => 3,
+                    'cat_id' => 'S003',
+                    'cat_name'=>'Food',
+                    'desc'=>'testing'
+                ]
+            ];
+            
+            $data=[
+                'data'=>$category
+            ];
+            return response()->json($data);
+        }
+
+}
